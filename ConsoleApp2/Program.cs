22a09e880d0194b5b76718ed121bd05fa7c8842f
MikiraSora <mikirasora0409@126.com>
2023-05-10 05:28:57 +0800

    add param useApplication

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OngekiFumenEditor;
+using OngekiFumenEditor.Utils;
+
+var app = new AppBootstrapper();
+
+Console.WriteLine("Hello, World!");
