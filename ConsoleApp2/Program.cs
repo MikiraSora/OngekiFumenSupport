434ff40c31ea752a10cf90d8f8d39d5c56331809
MikiraSora <mikirasora0409@126.com>
2023-05-15 07:03:08 +0800

    implement limited preview mode.

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,7 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Caliburn.Micro;
 using OngekiFumenEditor;
+using OngekiFumenEditor.Base;
+using OngekiFumenEditor.Base.Collections;
+using OngekiFumenEditor.Modules.FumenVisualEditor;
 using OngekiFumenEditor.Utils;
+using OngekiFumenEditorPlugins.OngekiFumenSupport;
+using System.IO;
 
-var app = new AppBootstrapper();
+var app = new AppBootstrapper(false);
 
-Console.WriteLine("Hello, World!");
+var parser = new DefaultOngekiFumenParser(IoC.GetAll<ICommandParser>());
+using var fs = File.OpenRead(@"F:\ongeki bright memory\package\option\A032\music\music8090\8090_10.ogkr");
+var fumen = await parser.DeserializeAsync(fs);
+
+var pot = fumen.Soflans.GetCachedSoflanPositionList_PreviewMode(240, fumen.BpmList);
+
+Console.WriteLine();
